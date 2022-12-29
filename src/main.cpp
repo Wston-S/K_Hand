c3da5428447b69e3bbd686aedfea94cb80f27f23
v <L15119696007@icloud.com>
2022-12-29 16:46:04 +0800

    --

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include "HAL/HAL.h"
 #include "Port/Display.h"
+#include "App/App.h"
 #include "tasks/inc/demo_task.h"
 #include "tasks/inc/sensor_task.h"
 #include "tasks/inc/net_task.h"
@@ -11,6 +12,7 @@ void setup()
 {
   HAL::Init();
   Port_Init();
+  App_Init();
 
   // sensor_task_init();
   // net_task_init();
